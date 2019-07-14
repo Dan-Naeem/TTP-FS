fdc92aad646cd5910995fbffa5996e5e28bf3350
Dan Naeem <dan.naeem95@gmail.com>
2019-07-14 19:26:17 -0400

    connect to database

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app  = express();
 
-//EJS
+// DB Config
+const db = require('./config/keys').MongoURI
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-//Routes
+// Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
