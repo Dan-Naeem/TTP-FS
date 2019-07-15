54854163f4cc35a71af5c8b5cf48e2dfd945e6e3
Dan Naeem <dan.naeem95@gmail.com>
2019-07-14 22:33:09 -0400

    flash message upon registration

--- routes/users.js
+++ routes/users.js
@@ -71,6 +71,7 @@ router.post('/register', (req, res) => {
                             // save user
                             newUser.save()
                             .then(user => {
+                                req.flash('success_msg', 'You are now registered');
                                 res.redirect('/users/login');
                             })
                             .catch(err => console.log(err));
