123d0550d0d53b6f7b2e41fa9f9fd94153f28848
Dan Naeem <dan.naeem95@gmail.com>
2019-07-16 14:32:37 -0400

    update and add new stock onto users.stock

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
+// User model
+const User = require('../models/User');
+
 // Welcome page
 router.get('/', (req, res) => res.render('welcome'));
 
@@ -22,5 +25,34 @@ router.get('/dashboard/search', ensureAuthenticated, (req, res) =>
   res.render('search', {
     user: req.user
   }));
+
+  // Search - post
+router.post('/dashboard/search', ensureAuthenticated, (req, res) => {
+  const email = req.user.email;
+  const { tickerSymbol, numberOfShares } = req.body;
+  User.findOne({ email: email })
+    .then(user => {
+      // if users exists (it should)
+      if(user) {
+        // create a stock object
+        let newStock = {
+          tickerSymbol: tickerSymbol,
+          numberOfShares: numberOfShares,
+        }
+        // update users stock profile (push)
+        user.stocks.push(newStock);
+        // save updated users profile
+        user.save()
+          .then(user => {
+            res.redirect('/dashboard');
+          })
+          .catch(err => console.log(err));
+      }
+      else {
+        console.log('user doesnt exist')
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
